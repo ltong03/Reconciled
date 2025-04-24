917f3228284619e683e355f2168ecfe6a5f23674
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-23 17:57:21 -0700

    Added cursor to show if player is looking at something interactable

--- Assets/Scripts/playerInteraction.cs
+++ Assets/Scripts/playerInteraction.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerInteraction : MonoBehaviour
 {
     public Transform orientation;
+    public Image cursor;
     public float interactDistance = 1f;
 
     Ray lookRay;
@@ -17,17 +19,28 @@ public class playerInteraction : MonoBehaviour
     void Update()
     {
         lookRay = new Ray(orientation.position, orientation.forward);
-        Debug.Log(getInteraction().collider);
+
+        if(canInteract() && Input.GetButtonDown("Interact"))
+            Debug.Log(getInteraction().collider);
+    }
+
+    bool canInteract()
+    {
+        if(Physics.Raycast(lookRay.origin, lookRay.direction, interactDistance))
+        {
+            cursor.color = Color.white;
+            return true;
+        }
+        else
+        {
+            cursor.color = Color.black;
+            return false;
+        }
     }
 
     RaycastHit getInteraction()
     {
-        if(Input.GetButtonDown("Interact"))
-        {
-            RaycastHit[] hits = Physics.RaycastAll(lookRay, interactDistance);
-            return hits[0];
-        }
-
-        return new RaycastHit();
+        RaycastHit[] hits = Physics.RaycastAll(lookRay, interactDistance);
+        return hits[0];
     }
 }
