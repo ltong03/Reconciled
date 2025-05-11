0e08794c22551e7dc5ef3153512691cd4354f0ff
Tshajlij Lee <tlee23@sfsu.edu>
2025-05-11 00:52:05 -0700

    Main menu adjustments, and I installed the SpriteEditor package to make editing sprites easier

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu:MonoBehaviour{
+    public Rigidbody pie;
+    public string gameSceneName;
 
-    public void PlayGame(){
+    public void KnockPieDown(){
+        pie.isKinematic = false;
+        pie.useGravity = true;
+        float forceRandomizer;
+
+        pie.AddForce(Vector3.forward * (1.7f + UnityEngine.Random.Range(-0.01f, 0.1f)) + Vector3.up * (2.7f + UnityEngine.Random.Range(-0.01f, 0.1f)), ForceMode.Impulse);
+
+        Invoke(nameof(StartGame), 5f);
+        
+    }
+    public void StartGame(){
         SceneManager.LoadSceneAsync("InteractTest");
     }
     
