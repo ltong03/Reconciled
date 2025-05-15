e95e705516ab6373ce6f5ad832c566ba702515d4
Tshajlij Lee <tlee23@sfsu.edu>
2025-05-15 01:23:24 -0700

    Added new pi mesh. It kinda has a lot of polygons, lol. Integrated pi into scene and generated physics based progression for the main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -19,7 +19,7 @@ public class MainMenu:MonoBehaviour{
 
         
         if(canPressStart){
-            pie.AddForce(Vector3.forward * (1.7f + UnityEngine.Random.Range(-0.01f, 0.1f)) + Vector3.up * (2.7f + UnityEngine.Random.Range(-0.01f, 0.1f)), ForceMode.Impulse);
+            pie.AddForce(Vector3.forward * (1.7f + UnityEngine.Random.Range(-0.01f, 0.1f)) + Vector3.up * (2.6f + UnityEngine.Random.Range(-0.01f, 0.3f)), ForceMode.Impulse);
             Invoke(nameof(StartGame), 3.5f);
             canPressStart = false;
         }
@@ -30,7 +30,10 @@ public class MainMenu:MonoBehaviour{
     public void StartGame(){
         //AkSoundEngine.PostEvent(postEventEndAmbience, gameObject);
         AkSoundEngine.ExecuteActionOnEvent(postEventStartAmbience, AkActionOnEventType.AkActionOnEventType_Stop, gameObject, 1000);
-        SceneManager.LoadSceneAsync(gameSceneName);
+        // Changing start of game to physics based collision of pie on collider. This is symbolic of the story. There is a small change the pie won't go out of the window. If this happens, the game won't start. Cool right? 
+        // Maybe we can transition to a secret ending if pie doesn't 
+        
+        //SceneManager.LoadSceneAsync(gameSceneName);
         
     }
     
